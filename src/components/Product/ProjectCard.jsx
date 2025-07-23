f3417d11c3211d18fb298282e44c06a05be50c44
Zainab Khayat <zainab224498@gmail.com>
2025-07-23 20:53:25 +0300

    Update ProjectCard.jsx

--- src/components/Product/ProjectCard.jsx
+++ src/components/Product/ProjectCard.jsx
@@ -9,19 +9,19 @@ export const ProjectCard = ({
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <div className={styles.left}>
-          <img
-            src={getImageUrl(imageSrc)}
-            alt={`Image of ${title}`}
-            className={styles.image}
-          />
-          <div className={styles.links}>
-            <a href={demo} className={styles.link} target="_blank">
-              Demo
-            </a>
+          <div className={styles.left}>
+            <img
+              src={getImageUrl(imageSrc)}
+              alt={`Image of ${title}`}
+              className={styles.image}
+            />
+            <div className={styles.links}>
+              <a href={demo} className={styles.link} target="_blank">
+                Demo
+              </a>
+            </div>
           </div>
-        </div>
-        <div>
+        <div className={styles.right}>
           <h3 className={styles.title}>{title}</h3>
           <p className={styles.description}>{description}</p>
           <ul className={styles.skills}>
