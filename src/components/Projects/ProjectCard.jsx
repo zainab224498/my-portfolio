25e44e8ef48918c84a94bf4c461697e133130134
Zainab Khayat <zainab224498@gmail.com>
2025-07-23 20:33:47 +0300

    Update ProjectCard.jsx

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -8,12 +8,21 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className={styles.container}>
-      <img
+      <div className={styles.box}>
+        <div className={styles.left}>
+                <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
         className={styles.image}
       />
-      <h3 className={styles.title}>{title}</h3>
+                <div className={styles.links}>
+        <a href={demo} className={styles.link} target="_blank">
+          Demo
+        </a>
+      </div>
+        </div>
+        <div className={styles.right}>
+                <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
         {skills.map((skill, id) => {
@@ -24,13 +33,7 @@ export const ProjectCard = ({
           );
         })}
       </ul>
-      <div className={styles.links}>
-        <a href={demo} className={styles.link} target="_blank">
-          Demo
-        </a>
-        <a href={source} className={styles.link} target="_blank">
-          Source
-        </a>
+        </div>
       </div>
     </div>
   );
