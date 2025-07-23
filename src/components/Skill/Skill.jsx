6af7fff1cdabd3b8007b2a9bb83da93677bb615b
Zainab Khayat <zainab224498@gmail.com>
2025-07-23 22:41:47 +0300

    Update Skill.jsx

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import styles from "./Skill.module.css";
 import skills from "../../data/skills.json";
-import productskills from "../../data/productskills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
@@ -10,21 +9,6 @@ export const Skill = () => {
   return (
     <section className={styles.container} id="skill">
       <h2 className={styles.title}>Skills</h2>
-      <h3 className={styles.titlesub}>Product Owner Skills</h3>
-      <div className={styles.content}>
-        <div className={styles.skills}>
-          {productskills.map((proskill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(proskill.imageSrc)} alt={skill.title} />
-                </div>
-                <p>{proskill.title}</p>
-              </div>
-            );
-          })}
-        </div>
-       <h3 className={styles.titlesub}>Frontend Skills</h3>
       <div className={styles.content}>
         <div className={styles.skills}>
           {skills.map((skill, id) => {
