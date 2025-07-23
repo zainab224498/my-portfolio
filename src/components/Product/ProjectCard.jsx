87a7a35019a73cf6fc36719a898ed67a043ce38f
Zainab Khayat <zainab224498@gmail.com>
2025-07-23 20:43:39 +0300

    Update ProjectCard.jsx

--- src/components/Product/ProjectCard.jsx
+++ src/components/Product/ProjectCard.jsx
@@ -8,30 +8,34 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className={styles.container}>
-      <img
-        src={getImageUrl(imageSrc)}
-        alt={`Image of ${title}`}
-        className={styles.image}
-      />
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.links}>
-        <a href={demo} className={styles.link} target="_blank">
-          Demo
-        </a>
-        <a href={source} className={styles.link} target="_blank">
-          Source
-        </a>
+      <div className={styles.box}>
+        <div>
+          <img
+            src={getImageUrl(imageSrc)}
+            alt={`Image of ${title}`}
+            className={styles.image}
+          />
+          <div className={styles.links}>
+            <a href={demo} className={styles.link} target="_blank">
+              Demo
+            </a>
+          </div>
+        </div>
+        <div>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.description}>{description}</p>
+          <ul className={styles.skills}>
+            {skills.map((skill, id) => {
+              return (
+                <li key={id} className={styles.skill}>
+                  {skill}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
+      
     </div>
   );
 };
