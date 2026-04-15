bcf869df3fa4d64f9eaea7f726d97b88ac4499cc
Zainab Khayat <zainab224498@gmail.com>
2026-04-15 13:18:28 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,13 +8,12 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Zainab Khayat</h1>
-        <h2 className={styles.subtitle}>PRODUCT OWNER</h2>
+        <h2 className={styles.subtitle}>E-commerce Product Owner & Strategic Growth Lead</h2>
         <p className={styles.description}>
-          Strategic Product Owner with 3+ years of technical frontend expertise.  
-Drives product vision, prioritizes backlogs, and delivers data-driven solutions by translating stakeholder needs into scalable products.  
-Optimizes workflows and aligns cross-functional teams using Agile methodologies to maximize business value. 
+          Strategic Product Owner & Software Engineer bridging the gap between technical precision and e-commerce growth.
+          With 6+ years of experience and 40+ stores developed, I transform platforms into profitable success stories." 
         </p>
-                <a href="https://drive.google.com/file/d/19bwbbKfcIj9AWv711aRAjpI433I4lb-G/view?usp=sharing" className={styles.contactBtn} target="_blank">
+                <a href="https://drive.google.com/file/d/1_4ShU8cUu-ekOGwO9EOidD8bJx1b5wOU/view?usp=sharing" className={styles.contactBtn} target="_blank">
           Check Resume
         </a>
       </div>
@@ -23,7 +22,7 @@ Optimizes workflows and aligns cross-functional teams using Agile methodologies 
         alt="Hero image of me"
         className={styles.heroImg}
       />
-      <div className={styles.topBlur} />
+      <div className={styles.topBlur} /
       <div className={styles.bottomBlur} />
     </section>
   );
